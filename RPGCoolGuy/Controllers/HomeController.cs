69cafef3142f0398ff5270c2fd34f2611c3dfbc5
Rohan Basnet <rohan.basnet@southeasttech.edu>
2024-10-09 14:23:54 -0500

    Add project files.

---
+++ RPGCoolGuy/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RPGCoolGuy.Models;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RPGCoolGuy.Controllers
+{
+    public class HomeController : Controller
+    {
+       private CharacterDBContext context {  get; set; }
+        
+       public HomeController(CharacterDBContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var characters = this.context.Characters.OrderBy(c => c.Name).ToList();
+            return View(characters);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View("CharacterForm");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var c = context.Characters.Find(id);
+            return View("CharacterForm", c);
+        }
+        [HttpPost]
+        public IActionResult Edit(Character c)
+        {
+            if (ModelState.IsValid)
+            {
+                if (c.Id == 0)
+                {
+                    context.Characters.Add(c);
+                }
+                else
+                {
+                    context.Characters.Remove(c);
+                }
+                context.SaveChanges();
+                var chars = context.Characters.ToList();
+                return View("Index", chars);
+            }
+            else 
+            {
+                return View("CharacterForm",c);
+            }
+           
+        }
+        public IActionResult Delete(int id)
+        {
+            Character del = context.Characters.Find(id);
+            context.Characters.Remove(del);
+            context.SaveChanges();
+            var c = context.Characters.ToList();
+            return View("Index", c);
+        }
+        [HttpGet]
+        public IActionResult Fight()
+        {
+            ViewBag.Characters = context.Characters.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Fight(Fight fight)
+        {
+            Character c1 = context.Characters.Find(fight.Character1);
+            Character c2 = context.Characters.Find(fight.Character2);
+            if(c1.Attack > c2.Attack)
+            {
+                ViewBag.result = c1.Name;
+            }
+            else if (c2.Attack > c1.Attack)
+            {
+                ViewBag.result = c2.Name;
+            }
+            else
+            {
+                ViewBag.result = "Tie";
+            }
+
+            return View("Result");
+        }
+    }
+}
---
+++ RPGCoolGuy/Migrations/20241007185354_InitialD.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RPGCoolGuy.Migrations
+{
+    public partial class InitialD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Attack = table.Column<int>(type: "int", nullable: true),
+                    HP = table.Column<int>(type: "int", nullable: true),
+                    Defense = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Characters",
+                columns: new[] { "Id", "Attack", "Defense", "HP", "Name" },
+                values: new object[] { 1, 20, 20, 5, "Dave" });
+
+            migrationBuilder.InsertData(
+                table: "Characters",
+                columns: new[] { "Id", "Attack", "Defense", "HP", "Name" },
+                values: new object[] { 2, 30, 30, 1, "Tony" });
+
+            migrationBuilder.InsertData(
+                table: "Characters",
+                columns: new[] { "Id", "Attack", "Defense", "HP", "Name" },
+                values: new object[] { 3, 99, 99, 9999, "Peter Griffin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Characters");
+        }
+    }
+}
